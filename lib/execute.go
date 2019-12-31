287972954d860253bd4a2391db7eea61ba595ff6
Craig Gardner <craig.s.gardner@gmail.com>
2019-12-30 22:16:30 -0800

    chore(lib): removed fmt/logging from execute function

--- lib/execute.go
+++ lib/execute.go
@@ -1,14 +1,11 @@
 package lib
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 )
 
 func Execute(command string, args ...string) error {
-	fmt.Println("Command:", command, "args:", args)
-
 	cmd := exec.Command(command, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
